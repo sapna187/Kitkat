06e7404ac032421902bedb1d4edf0b4d8a8cdf77
Sapna <96979796+sapna187@users.noreply.github.com>
2022-05-06 19:36:44 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -2,24 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Example extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+//  List<String> _locations = ['Please choose a location', 'A', 'B', 'C', 'D']; // Option 1
+//  String _selectedLocation = 'Please choose a location'; // Option 1
+  List<String> _locations = ['A', 'B', 'C', 'D']; // Option 2
+  String? _selectedLocation; // Option 2
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.purple),
-        home: Scaffold(
-          body: Container(
-            child: Column(
-              children: [
-                Text('Sapna')
-              ],
-            ),
+    return Scaffold(
+        body: Center(
+          child: DropdownButton(
+            hint: Text('Please choose a location'), // Not necessary for Option 1
+            value: _selectedLocation,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedLocation = newValue as String;
+              });
+            },
+            items: _locations.map((location) {
+              return DropdownMenuItem(
+                child: new Text(location),
+                value: location,
+              );
+            }).toList(),
           ),
-        )
 
+      ),
     );
   }
 }
